3161f81447ebfe53a2f6562ddd9f7d9f433bfc68
Stévillis Sousa <stevillis@hotmail.com>
2022-04-19 23:52:01 -0400

    feature: docstring

--- sum.py
+++ sum.py
@@ -1,8 +1,25 @@
-def is_even(num):
+def is_even(num: int) -> bool:
+    """Check if a given num is even.
+
+    Parameters:
+    num (int): An integer to be evaluated as even or not.
+
+    Returns:
+    bool: True if num is even, False otherwise.
+   """
     return num % 2 == 0
 
 
-def odd_sum(start, end):
+def odd_sum(start: int, end: int) -> int:
+    """Sum odd integers between start and end parameters.
+
+    Parameters:
+    start (int): Start range of odd integer sum.
+    end (int): End range of odd integer sum.
+
+    Returns:
+    int: Sum of odd numbers between start and end.
+   """
     sum = 0
     for num in range(start, end + 1):
         if not is_even(num):
